30c0ccc553dbc49654534370276bc3405e531ec2
Ishaan <ishaan20068@iiitd.ac.in>
2022-03-16 18:27:51 +0530

    pointers

--- pointers.cpp
+++ pointers.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 int main(){
     int i=0;
-    cout<<&i<<endl;
+    cout<<&i<<endl;//& gives the address of any variable
     int*p=&i;
-    cout<<*p<<endl;
+    cout<<*p<<endl;//* used infront of a pointer deferences the value written at the location pointed by pointer
     return 0;
 }
