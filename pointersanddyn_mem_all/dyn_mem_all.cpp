1db6e41d9425131c6d5eac249f0a35e013a86d3b
Ishaan <ishaan20068@iiitd.ac.in>
2022-03-17 03:43:28 +0530

    dma

---
+++ pointersanddyn_mem_all/dyn_mem_all.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int *p=new int;//dynamically allocated memory
+    delete p;//can't delete itself so it is essential to delete it manually else it will occupy whole memory
+    int* p1=new int[50];
+    for(int i=0;i<50;i++){
+        p1[i]=i;
+    }
+    for(int i=0;i<50;i++){
+        cout<<p1[i]<<endl;
+    }
+    delete [] p1;
+    for(int i=0;i<50;i++){
+        cout<<p1[i]<<endl;
+    }
+    return 0;
+}
