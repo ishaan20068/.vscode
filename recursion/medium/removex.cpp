17cb9db0f369f0cabdec00826a0f9e3f73ea78e5
Ishaan <ishaan20068@iiitd.ac.in>
2022-03-18 22:38:35 +0530

    rec

---
+++ recursion/medium/removex.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast            ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int             long long
+#define o(x)            cout<<x<<endl;
+string f(string x,int i){
+    if(i==x.size()){return "";}
+    else{
+        if(x[i]=='x'){return f(x,i+1);}
+        else{return x[i]+f(x,i+1);}}}
+void ans(){
+    string n;
+    cin>>n;
+    o(f(n,0));
+}
+int32_t main(){
+    fast
+    int t;cin>>t;
+    //int t=1
+    for(int i=0;i<t;i++){ans();}
+    return 0;
+}
