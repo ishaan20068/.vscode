929518c88c9be1a6264c829b860d0c2b262982d6
Ishaan <ishaan20068@iiitd.ac.in>
2022-03-18 16:07:46 +0530

    rec

---
+++ recursion/medium/allindex.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast            ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int             long long
+#define fr(i,n)         for(int i=0;i<n;i++)
+#define in              cin>>
+#define o(x)            cout<<x<<" ";
+#define mk(a,n,type)    type *a=new type[n];
+
+void f(int *a,int i,int n, int k){
+    if(i==n){
+        return ;
+    }
+    if(a[i]==k){o(i);}
+    f(a,i+1,n,k);
+}
+void ans(){
+    int n;
+    cin>>n;
+    int k;
+    in k;
+    mk(a,n,int)
+    fr(i,n){
+        in a[i];
+    }
+    f(a,0,n,k);
+}
+int32_t main(){
+    fast
+    int t;cin>>t;
+    //int t=1
+    for(int i=0;i<t;i++){ans();}
+    return 0;
+}
---
+++ recursion/medium/palindrome.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define fast            ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
+#define int             long long
+#define in              cin>>
+#define o(x)            cout<<x<<endl;
+
+bool f(string x,int i,int j){
+    if(i==j){return true;}
+    else if(j==i+1){return (x[i]==x[j]);}
+    else{return ((x[i]==x[j]) && (f(x,i+1,j-1)));}}
+void ans(){
+    string x;in x;
+    string k=(f(x,0,x.length()-1))?"true":"false";
+    o(k);
+}
+int32_t main(){
+    fast
+    int t;cin>>t;
+    //int t=1
+    for(int i=0;i<t;i++){ans();}
+    return 0;
+}
